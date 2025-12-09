645617c8e5f4d9802c17b31f55378cee503bc9c3
sumityadav04 <165358831+sumityadav04@users.noreply.github.com>
2025-12-09 13:49:17 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import AddJob from "./pages/AddJob";
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <Dashboard />
-    </div>
+    <BrowserRouter>
+      <div className="bg-gray-100 min-h-screen">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add-job" element={<AddJob />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
